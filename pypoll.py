fd284742a9cecf54b934d81d341eb72dcb391e8f
taviosammy <taviosammy@gmail.com>
2022-03-20 22:12:36 -0400

    update pypoll.py

--- pypoll.py
+++ pypoll.py
@@ -15,16 +15,35 @@ data_to_retrieve = os.path.join("Resources", "election_results.csv")
 # Create a filename variable to a direct or indirect path to the file.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
+#initializing an accumulator
+Total_votes = 0
+candidate_options = []
 # Open the election results and read the file.
 with open(data_to_retrieve) as election_data:
+
 #To do: perform analysis.
     
 # Read the file object with the reader function.
     file_reader = csv.reader(election_data)
 
-# Print the header row.
+# Read the header row.
     headers = next(file_reader)
-    print(headers)
+
+    # Print each row in the CSV file.
+    for row in file_reader:
+       # Print the candidate name from each row.
+        candidate_name = row[2]
+        # If the candidate does not match any existing candidate... 
+        if candidate_name not in candidate_options:
+             # Add it to the list of candidates
+            candidate_options.append(candidate_name)
+
+ #Print the candidate list.
+print(candidate_options)
+
+
+
+
 
 
 
